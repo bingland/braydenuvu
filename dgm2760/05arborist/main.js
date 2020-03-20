97ceef7ff7b9d9ac7396de52ab2981e4a125d8d4
Brayden England <bray.patriot@gmail.com>
2020-03-19 20:20:00 -0600

    started assignment 05

---
+++ dgm2760/05arborist/main.js
@@ -0,0 +1,6 @@
+// header / subheader
+const header = "The Aborist"
+const subheader = "Events and Arrays"
+document.querySelector('header h1').innerHTML = header
+document.querySelector('header h2').innerHTML = subheader
+
