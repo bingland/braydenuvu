1c4baed79ec5b63701c0367ffdf5237602f25828
Brayden England <bray.patriot@gmail.com>
2020-04-24 19:43:39 -0600

    Worked on GSAP project, added 2 animations.

--- dgm2780/GSAPintro/js/main.js
+++ dgm2780/GSAPintro/js/main.js
@@ -6,3 +6,18 @@ const hiddenNav = $('.hiddenNav')
 hamburger.click(() => {
     hiddenNav.toggle()
 })
+
+// fade in for header background
+const landing = $('header, .steps')
+gsap.from(landing, {duration: 2, opacity: 0, ease: 'power1'})
+
+// fade in for header text
+const landingHeader = $('.headerText h1')
+const landingText = $('.headerText p, .headerText button')
+gsap.from(landingHeader, {duration: 1, delay: 0.5, opacity: 0, y: 20})
+gsap.from(landingText, {duration: 1, delay: 1.5, opacity: 0, y: 20})
+
+// hover onto bottom two panels
+$('.biPanel').hover( function () {
+    console.log('hi lol')
+})
