250bb5824455b393a77963e5c25129347f52a19f
Brayden England <bray.patriot@gmail.com>
2020-04-25 15:55:00 -0600

    Finished GSP project for DGM 2780

--- dgm2780/GSAPintro/js/main.js
+++ dgm2780/GSAPintro/js/main.js
@@ -7,6 +7,8 @@ hamburger.click(() => {
     hiddenNav.toggle()
 })
 
+// GSAP ASSIGNMENT
+
 // fade in for header background
 const landing = $('header, .steps')
 gsap.from(landing, {duration: 2, opacity: 0, ease: 'power1'})
@@ -17,7 +19,16 @@ const landingText = $('.headerText p, .headerText button')
 gsap.from(landingHeader, {duration: 1, delay: 0.5, opacity: 0, y: 20})
 gsap.from(landingText, {duration: 1, delay: 1.5, opacity: 0, y: 20})
 
-// hover onto bottom two panels
-$('.biPanel').hover( function () {
-    console.log('hi lol')
+// hover onto "how it works" icons
+const stepImages = $('.stepImage').toArray()
+console.log(stepImages)
+stepImages.forEach((myimg, index) => {
+    $(myimg).hover(function () {
+        // hover on
+        gsap.to(myimg, {duration: 0.5, y: -5, ease: 'power3'})
+    },
+    function () {
+        // hover out
+        gsap.to(myimg, {duration: 0.5, y: 5, ease: 'power3'})
+    })
 })
