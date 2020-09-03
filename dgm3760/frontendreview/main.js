24e5344ac9045f0c78c07e480e220fe14a7b4dcd
Brayden England <bray.patriot@gmail.com>
2020-09-02 19:44:27 -0600

    Hooked up app to Blizzard API

--- dgm3760/frontendreview/main.js
+++ dgm3760/frontendreview/main.js
@@ -1,20 +1,47 @@
-const getData = () => {
-    fetch("https://omgvamp-hearthstone-v1.p.rapidapi.com/info", {
+let data = null
+
+const output = document.querySelector('#output')
+
+// access token US6hAHXoJtO5a7BM0xGQYiQJaxWvNr4Jvo
+
+const init = () => {
+    let baseURL = 'https://us.api.blizzard.com/hearthstone/cards?locale=en_US'
+    let params = '&minionType=beast&page=2'
+    let accessToken = '&access_token=USO9BHzET8t2u4jjILrXkXmCjmho1gDOMD'
+    let fullURL = baseURL + params + accessToken
+    console.log('%c ' + fullURL, 'font-weight: bold;')
+
+    fetch(fullURL, {
         "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
-            "x-rapidapi-key": "cf523f5a76msh4689799707fda14p19eaddjsn26bd7e314fff"
-        }
     })
     .then(response => {
         return response.json()
     })
     .then(response => {
-        console.log(response)
+        data = response
+        display()
     })
     .catch(err => {
         console.log(err);
     });
 }
 
-getData()
+const display = () => {
+    console.log(data)
+
+    // meta into
+    output.innerHTML += `
+    <p>${data.cardCount} cards - Page ${data.page} out of ${data.pageCount}</p>
+    `
+    data.cards.forEach(element => {
+        output.innerHTML += `
+        <div class="card">
+            <p>${element.name} - Mana: ${element.manaCost}</p>
+            <img src="${element.cropImage}">
+        </div>`
+    });
+}
+
+init()
+
+// curl -u a19ed0f786f4495b98d0b6d249063806:FDbRUwO5ado8vUq6fwJ0P3UXPXx7y24u -d grant_type=client_credentials https://us.battle.net/oauth/token
